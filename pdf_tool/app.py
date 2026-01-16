19ad820558622d9ddc48ff8a0f7910926ef41105
webillion <dainox777@gmail.com>
2026-01-16 20:00:47 +0900

    Update app.py

--- pdf_tool/app.py
+++ pdf_tool/app.py
@@ -87,4 +87,5 @@ def check_auth():
         return jsonify({'status': 'error', 'message': 'パスワードが違います'}), 403
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
